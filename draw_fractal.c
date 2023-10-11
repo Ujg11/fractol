f8ef4ab3ad47cec9f1822b2f24e116a53ed60129
Ujg11 <orioljg2002@gmail.com>
2023-10-11 13:02:08 +0200

    Julia implementat

--- draw_fractal.c
+++ draw_fractal.c
@@ -21,10 +21,36 @@ void	recalculate_fract(t_fractal *f, double zoom, int x, int y)
 	draw_fractal(f, f->fract, f->params);
 }
 
-void	draw_julia(t_fractal *f, char **params)
+void	draw_julia(t_fractal *f, char **params, t_imag *imag)
 {
-	(void)f;
-	(void)params;
+	int		i;
+	double	x_tmp;
+	//(void)f;
+	//(void)params;
+	f->cx = ft_atoi(params[2]);
+	f->cy = ft_atoi(params[3]);
+	f->zx = -2.0 + (double)f->x * (2.0 - (-2.0)) / WIDTH;
+	f->zy = f->cy = 2.0 + (double)f->y * (-2.0 - 2.0) / HEIGHT;
+	while (++i < f->max_iterations)
+	{
+		x_tmp = f->zx * f->zx - f->zy * f->zy + f->cx;
+		f->zy = (2. * f->zx * f->zy + f->cy) * f->zoom;
+		f->zx = x_tmp * f->zoom;
+		if (f->zx * f->zx + f->zy * f->zy >= 4)
+			break ;
+	}
+	if (i == f->max_iterations)
+	{
+		//mlx_pixel_put(f->mlx, f->win, f->x, f->y, 0x000000);
+		put_color_to_pixel(f, imag, 0xFFFFFF);
+	}
+	else
+	{
+		put_color_to_pixel(f, imag, (i+456) * 999999);
+		//(void)imag;
+		//mlx_pixel_put(f->mlx, f->win, f->x, f->y, (i + 20) * 1111111);
+	}
+
 }
 
 // De la formula varia la C y Z0 = 0
@@ -68,7 +94,7 @@ void	draw_fractal(t_fractal *f, int fract, char **params)
 		while (f->y < HEIGHT)
 		{
 			if (fract == 1)
-				draw_julia(f, params);
+				draw_julia(f, params, f->imag);
 			else if (fract == 2)
 				draw_mandelbrot(f, f->imag);
 			f->y++;
